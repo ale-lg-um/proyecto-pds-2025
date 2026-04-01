234cd0452f25912670e08f28df6cc95e3a8b9820
Daniel Marín Sánchez <danielmarinsanchez6@gmail.com>
2026-04-01 13:53:49 +0200

    Prueba de correo eliminada

--- workspace-proyecto/proyecto-tarjetas/src/main/java/es/um/pds/tarjetas/ProyectoTarjetasApplication.java
+++ workspace-proyecto/proyecto-tarjetas/src/main/java/es/um/pds/tarjetas/ProyectoTarjetasApplication.java
@@ -1,13 +1,8 @@
 package es.um.pds.tarjetas;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import es.um.pds.tarjetas.domain.model.usuario.id.UsuarioId;
-import es.um.pds.tarjetas.domain.ports.output.PuertoEnvioEmail;
 
 //import es.um.pds.tarjetas.ui.App;
 //import javafx.application.Application;
@@ -21,18 +16,4 @@ public class ProyectoTarjetasApplication {
 		
 		//Application.launch(App.class, args);
 	}
-	
-	// Prueba del correo:
-    @Bean
-    CommandLineRunner testEmail(PuertoEnvioEmail puertoEnvioEmail) {
-        return args -> {
-            puertoEnvioEmail.enviarEmail(
-                UsuarioId.of("danielmarinsanchez6@gmail.com"),
-                "Test desde Spring Boot",
-                "Hola, este es un email de prueba"
-            );
-        };
-    }
- 
-
 }
