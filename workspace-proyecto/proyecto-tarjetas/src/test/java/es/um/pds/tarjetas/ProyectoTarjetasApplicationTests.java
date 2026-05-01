4bfec0e5dd29a7e43947d722ce609132bd09d6fc
Daniel Marín Sánchez <danielmarinsanchez6@gmail.com>
2026-05-01 13:26:39 +0200

    Crear mock del puerto de envío email en ApplicationTests

--- workspace-proyecto/proyecto-tarjetas/src/test/java/es/um/pds/tarjetas/ProyectoTarjetasApplicationTests.java
+++ workspace-proyecto/proyecto-tarjetas/src/test/java/es/um/pds/tarjetas/ProyectoTarjetasApplicationTests.java
@@ -2,10 +2,17 @@ package es.um.pds.tarjetas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import es.um.pds.tarjetas.domain.ports.output.PuertoEnvioEmail;
 
 @SpringBootTest
 class ProyectoTarjetasApplicationTests {
 
+	// Necesario para que funcione la GitHub action
+	@MockBean
+	private PuertoEnvioEmail puertoEnvioEmail;
+	
 	@Test
 	void contextLoads() {
 	}
