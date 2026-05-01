19667ef14facd552d7176cd4fe5b8b4082aed93e
Daniel Marín Sánchez <danielmarinsanchez6@gmail.com>
2026-05-01 13:38:30 +0200

    ProyectoTarjetasApplicationTests mockeado también con el correo y añadidas propiedades del test

--- workspace-proyecto/proyecto-tarjetas/src/test/java/es/um/pds/tarjetas/ProyectoTarjetasApplicationTests.java
+++ workspace-proyecto/proyecto-tarjetas/src/test/java/es/um/pds/tarjetas/ProyectoTarjetasApplicationTests.java
@@ -3,15 +3,27 @@ package es.um.pds.tarjetas;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import es.um.pds.tarjetas.domain.ports.output.PuertoEnvioEmail;
 
+//Levanta el contexto de la aplicación con test SpringBoot
 @SpringBootTest
+
+// Activa el perfil test de Spring, porque se pueden tener distintos entornos
+@ActiveProfiles("test")
+
+//Propiedades específicas para el test
+@TestPropertySource(properties = { "spring.datasource.url=jdbc:h2:mem:tarjetasdb_test;DB_CLOSE_DELAY=-1",
+		"spring.datasource.driverClassName=org.h2.Driver", "spring.datasource.username=sa",
+		"spring.datasource.password=", "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+		"spring.jpa.hibernate.ddl-auto=create-drop", "spring.jpa.show-sql=false",
+		"spring.mail.username=test@ejemplo.com" })
 class ProyectoTarjetasApplicationTests {
 
-	// Necesario para que funcione la GitHub action
 	@MockBean
-	private PuertoEnvioEmail puertoEnvioEmail;
+	PuertoEnvioEmail puertoEnvioEmail;
 	
 	@Test
 	void contextLoads() {
