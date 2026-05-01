8fa852a64d988ffb63ac624d23df73965acbe302
Daniel Marín Sánchez <danielmarinsanchez6@gmail.com>
2026-05-01 14:57:48 +0200

    Fix en el test de servicio de autenticación

--- workspace-proyecto/proyecto-tarjetas/src/test/java/es/um/pds/tarjetas/application/ServicioAutenticacionImplTest.java
+++ workspace-proyecto/proyecto-tarjetas/src/test/java/es/um/pds/tarjetas/application/ServicioAutenticacionImplTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import es.um.pds.tarjetas.domain.model.usuario.id.UsuarioId;
 import es.um.pds.tarjetas.domain.ports.input.ServicioAutenticacion;
@@ -26,6 +27,14 @@ import jakarta.transaction.Transactional;
 
 //Activa el perfil test de Spring, porque se pueden tener distintos entornos
 @ActiveProfiles("test")
+
+//Propiedades específicas para este test
+@TestPropertySource(properties = { "spring.datasource.url=jdbc:h2:mem:tarjetasdb_test;DB_CLOSE_DELAY=-1",
+		"spring.datasource.driverClassName=org.h2.Driver", "spring.datasource.username=sa",
+		"spring.datasource.password=", "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+		"spring.jpa.hibernate.ddl-auto=create-drop", "spring.jpa.show-sql=false",
+		"spring.mail.username=test@ejemplo.com" })
+
 class ServicioAutenticacionImplTest {
 
 	@Autowired
